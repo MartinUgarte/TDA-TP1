f15cfb63a0def59bd1836df2755e32ef4ff4fee2
MartinUgarte <39602138+MartinUgarte@users.noreply.github.com>
2023-04-25 02:44:11 -0300

    graficos

--- k_merge_dyc.py
+++ k_merge_dyc.py
@@ -1,15 +1,15 @@
 # K-MERGE por División y Conquista
 import random
 
-def k_merge(arreglos_ordenados):
+def k_merge_dyc(arreglos_ordenados):
     if len(arreglos_ordenados) == 0:
         return []
     if len(arreglos_ordenados) == 1:
         return arreglos_ordenados[0]
     
     medio = len(arreglos_ordenados) // 2
-    arreglo_uno = k_merge(arreglos_ordenados[0: medio])
-    arreglo_dos = k_merge(arreglos_ordenados[medio: len(arreglos_ordenados)])
+    arreglo_uno = k_merge_dyc(arreglos_ordenados[0: medio])
+    arreglo_dos = k_merge_dyc(arreglos_ordenados[medio: len(arreglos_ordenados)])
 
     return mergear(arreglo_uno, arreglo_dos)
 
@@ -29,26 +29,24 @@ def mergear(arreglo_uno, arreglo_dos):
             i += 1
         else:
             arreglo_resultado.append(arreglo_dos[j])
-            j += 1
-        
+            j += 1 
     
     return arreglo_resultado
 
 # - TESTS - #
 
-l0 = []
-l1 = [1, 3, 5, 7, 9]
-l2 = [2, 4, 6, 8, 10]
-l3 = [1, 13, 15, 17, 19]
+def main():
 
-assert k_merge(l0) == []
-assert k_merge([l1]) == l1
-assert k_merge([l0,l1]) == l1
-assert k_merge([l1,l2]) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-assert k_merge([l1, l2, l3]) == [1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 13, 15, 17, 19]
-assert k_merge([l1, l1]) == [1, 1, 3, 3, 5, 5, 7, 7, 9, 9]
+    l0 = []
+    l1 = [1, 3, 5, 7, 9]
+    l2 = [2, 4, 6, 8, 10]
+    l3 = [1, 13, 15, 17, 19]
 
-l4 = [random.randint(0, 999) for i in range(10000)]
-l5 = [random.randint(0, 999) for i in range(10000)]
+    assert k_merge_dyc(l0) == []
+    assert k_merge_dyc([l1]) == l1
+    assert k_merge_dyc([l0,l1]) == l1
+    assert k_merge_dyc([l1,l2]) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    assert k_merge_dyc([l1, l2, l3]) == [1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 13, 15, 17, 19]
+    assert k_merge_dyc([l1, l1]) == [1, 1, 3, 3, 5, 5, 7, 7, 9, 9]
 
-# TODO: Test de volumen
+main()
